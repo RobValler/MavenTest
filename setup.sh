d9fb75f0ef1c7a4c696a7b969a224676d7b75e69
RobValler <r.valler@yahoo.co.uk>
2020-06-26 01:31:10 +0200

    Update setup script to install all jars
    script.sh will now install all jars into the
    internal maven repo.
    This includes jars in all subdirectories.

--- setup.sh
+++ setup.sh
@@ -1,19 +1,37 @@
 #!/bin/bash -e
 
-mvn install:install-file \
-   -Dfile=jars/JarTest-1.0-SNAPSHOT.jar \
-   -DgroupId=com.mylib \
-   -DartifactId=mylib-core \
-   -Dversion=0.0.1 \
-   -Dpackaging=jar \
-   -DgeneratePom=true
+GROUPID="com.mystuff"
+ARTIFACTPREFIX="support"
+VERSION="0.0.1"
 
-mvn install:install-file \
-   -Dfile=jars/JarTest_MOO-1.0-SNAPSHOT.jar \
-   -DgroupId=com.mylib \
-   -DartifactId=mylib-core-moo \
-   -Dversion=0.0.1 \
-   -Dpackaging=jar \
-   -DgeneratePom=true
+function install()
+{
+     # find the filename, without path and extension
+     filename=$(basename -- "$1")
+     extension="${filename##*.}"
+     filename="${filename%.*}"
+     
+     # install the jar into the local maven repo
+     mvn install:install-file \
+        -Dfile=$1 \
+        -DgroupId=$GROUPID \
+        -DartifactId=$ARTIFACTPREFIX.$filename \
+        -Dversion=$VERSION \
+        -Dpackaging=jar \
+        -DgeneratePom=true
+}
 
+function test()
+{
+     filename=$(basename -- "$1")
+     extension="${filename##*.}"
+     filename="${filename%.*}"
+     echo $filename
+}
+
+# check all sub directories for jar files
+for result in $(find . -name '*.jar'); do
+     #install $result
+     test $result
+done
 
